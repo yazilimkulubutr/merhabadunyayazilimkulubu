386ad0c37f07f120d66f4ce0b997da48641e27db
yazilimkulubutr <yazilimkulubu@hotmail.com>
2015-12-04 20:56:04 +0200

    Start

---
+++ merhabadunyayazilimkulubu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merhabadunyayazilimkulubu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tek Satır Yorum Satırı
+
+            /*
+            Çoklu Yorum Satırı
+           */
+
+            Console.WriteLine(3 + 4);
+            Console.WriteLine(9 - 3);
+            Console.WriteLine(9 * 4);
+            Console.WriteLine(18 / 6);
+            Console.WriteLine("3+4");
+            Console.ReadLine();
+
+        }
+    }
+}
